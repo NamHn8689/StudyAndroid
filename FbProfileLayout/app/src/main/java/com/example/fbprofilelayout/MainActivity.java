8173b769c5e0477e41fc8a367cddf2af90fbbad2
namkeke <phuongnamhn2000@gmail.com>
2020-03-10 00:31:18 +0700

    10/3/2020

--- FbProfileLayout/app/src/main/java/com/example/fbprofilelayout/MainActivity.java
+++ FbProfileLayout/app/src/main/java/com/example/fbprofilelayout/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.fbprofilelayout;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
+
+    ImageView imageView;
+    RoundImage roudndImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.rounldImage);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.avatar);
+        roudndImage = new RoundImage(bitmap);
+        imageView.setImageDrawable(roudndImage);
     }
 }
